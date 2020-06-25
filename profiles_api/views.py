fa369f631e095c91c1a507c04c42a8c4b8497eb0
Ajay <ajay.kale61@gmail.com>
2020-06-25 21:00:30 +0530

    added hello api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test api view"""
+    
+    def get(self, request, format=None):
+        """Returns a list of apiview features"""
+       
+        an_apiview = [
+            'uses http methods',
+            'hi this is ajay',
+            'sdasd sdas'
+        ]
+
+        return Response({'message':'Hello!','an_apiview': an_apiview})
